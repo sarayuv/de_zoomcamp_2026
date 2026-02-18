3f49cb3ab946b985a03edaba5abbfcebb60c24b3
Sarayu Vijayanagaram <147265985+sarayuv@users.noreply.github.com>
2026-02-18 20:34:56 +0000

    Refactor trip union logic to enhance clarity; add yellow trip data handling and remove obsolete dimensions

--- Homeworks/homework4/models/marts/dim_vendors.sql
+++ Homeworks/homework4/models/marts/dim_vendors.sql
@@ -1,1 +1,11 @@
-select 1
+with trips_unioned as (
+    select * from {{ ref('int_trips_unioned')}}
+),
+
+vendors as (
+    select
+        distinct vendor_id
+    from trips_unioned
+)
+
+select * from vendors
